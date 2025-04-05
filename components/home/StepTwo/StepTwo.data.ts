d6439e1be7c919e3d599db5295476a7d85c99e5a
Jonathan Freites <jonathanfreites@gmail.com>
2025-04-05 09:37:46 -0600

    user config step two

--- components/home/StepTwo/StepTwo.data.ts
+++ components/home/StepTwo/StepTwo.data.ts
@@ -1,22 +1,42 @@
-export const dataCreator = [
+export const data = [
     {
-        title: "💫 Creator",
-        value: "creator", 
+        icon: "https://cggozb8yqe.ufs.sh/f/bpEOpEJjI8zKkTQYX0UbvxGfpCkRUEDoT2gJOwm7aYNy9tzq",
+        name: "LinkdIn", 
+        url: ""
     },
     {
-        title: "🏢 Business",
-        value: "business",
+        icon: "https://cggozb8yqe.ufs.sh/f/bpEOpEJjI8zKrUZIs7j5AacnPu96M7ZlOFNervmCTibXsHUI",
+        name: "YouTube", 
+        url: ""
     },
     {
-        title: "👨🏽‍💻 Developer",
-        value: "Developer",
+        icon: "https://cggozb8yqe.ufs.sh/f/bpEOpEJjI8zKNdKQYSeXGdac1fRlsKjYBwt5FIuQWCZ3qvhx",
+        name: "Twitter",
+        url: ""
     },
     {
-        title: "🏠 Real State",
-        value: "real-state",
+        icon: "https://cggozb8yqe.ufs.sh/f/bpEOpEJjI8zKC8w7gynmyTVpuZKtF50UHfODdBAn2zRr96Mh",
+        name: "Facebook", // Facebook is still relevant for many users, so it should be included
+        url: ""
     },
     {
-        title: "🌐 Other",
-        value: "other",
+        icon: "https://cggozb8yqe.ufs.sh/f/bpEOpEJjI8zKSjStVGfW7v9zCN5RoAJ0GpaugjtHVPDFci8Z",
+        name: "Dribble",
+        url: ""
+    },
+    {
+        icon: "https://cggozb8yqe.ufs.sh/f/bpEOpEJjI8zKrUQT8Ze5AacnPu96M7ZlOFNervmCTibXsHUI",
+        name: "Pinterest",
+        url: ""
+    },
+    {
+        icon: "https://cggozb8yqe.ufs.sh/f/bpEOpEJjI8zKJc6p1GzZyzNdx8P1WmLFlMg9OHwYXkR5qa0B",
+        name: "Twitch",
+        url: ""
+    },
+    {
+        icon: "https://cggozb8yqe.ufs.sh/f/bpEOpEJjI8zKXOZpd8geA8sR5XfGob7MElYKJxrvLdFWBgjt",
+        name: "TikTok",
+        url: ""
     }
 ]
--- components/home/StepTwo/StepTwo.tsx
+++ components/home/StepTwo/StepTwo.tsx
@@ -1,21 +1,59 @@
 import { Button } from "@/components/ui/button";
-import { dataCreator } from "./StepTwo.data";
+import { data } from "./StepTwo.data";
 import { useStepConfig } from "@/hooks/use-step-config";
+import Image from "next/image";
+import { useState } from "react";
 
 export function StepTwo() {
-  const { setInfoUser, nextStep } = useStepConfig()
+  const { setInfoUser, infoUser } = useStepConfig()
+
+  const [selectedPlatforms, setSelectedPlatforms] = useState<string[]>(
+    infoUser?.platforms?.map((platform) => platform.name) || []
+  )
+
+  const handleSelectPlatform = (platform: string) => {
+    setSelectedPlatforms((prevSelected) => {
+      if (prevSelected.includes(platform)) {
+        // If already selected, remove it from the array
+        return prevSelected.filter((item) => item !== platform)
+      } else {
+        // If not selected, add it to the array
+        return [...prevSelected, platform]
+      }
+    })
+  }
+
+  const handleContinue = () => {
+    setInfoUser((prevInfoUser) => ({
+      ...prevInfoUser,
+      platforms: data.filter(({ name }) => {
+        // Filter the platforms based on the selected names
+        selectedPlatforms.includes(name)
+      })
+    }))
+  }
 
   return (
     <div>
       <h2 className="text-center font-semibold text-2xl">Which platform are you on?</h2>
       <p className="text-center">Pick the ones that you are on.</p>
       
-      <div className="grid grid-cols-1 gap-2 mt-4">
-        <p>iconos</p>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-x-8 gap-y-4 mt-4 bg-white">
+        {data.map(({ icon, name }) => (
+          <div 
+            key={name} 
+            className={`flex flex-col gap-1 items-center rounded-lg py-3 hover:bg-violet-300 transition-all duration-300 cursor-pointer 
+              ${selectedPlatforms.includes(name) ? 'bg-violet-700 text-white' : 'bg-slate-200 text-violet-900'}`}
+            onClick={() => handleSelectPlatform(name)}
+          >
+            <Image src={icon} alt={name} width={40} height={40} />
+            <p className="text-sm">{name}</p>
+          </div>
+        ))}
       </div>
 
       <div className="mt-6">
-        <Button className="w-full bg-purple-600" onClick={nextStep}>Continue</Button>
+        <Button className="w-full bg-purple-600" onClick={handleContinue}>Continue</Button>
       </div>
     </div>
   )
