2856ace49054a681de57743d1483489b7f451245
Jonathan Freites <jonathanfreites@gmail.com>
2025-04-08 11:25:59 -0600

    show social links in the preview

--- components/home/ProfilePreview/PhonePreview/SocialNetworkList/SocialNetworkList.tsx
+++ components/home/ProfilePreview/PhonePreview/SocialNetworkList/SocialNetworkList.tsx
@@ -1,6 +1,22 @@
+import { useUserInfo } from "@/hooks/use-user-info"
+import Image from "next/image"
+
+
 export function SocialNetworkList() {
+  const {links} = useUserInfo()
+
+  if (!links) return null
+
   return (
-    <div>SocialNetworkList</div>
+    <ul className="grid grid-cols-2 md:grid-cols-4 gap-3 mt-3">
+        {links?.map(link => (
+            <li key={link.id}>
+                <a href={link.url || ''} target="_blank" rel="noreferrer">
+                    <Image src={link.icon || ''} alt="icon" width={40} height={40} className="bg-white rounded-full object-cover aspect-square" />
+                </a>
+            </li>
+        ))}
+    </ul>
   )
 }
 
